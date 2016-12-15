576c2776212882d8678341a122e377f0bfe7feda
Wesley Handy <wesleylhandy@gmail.com>
2016-12-15 18:37:40 -0500

    added basic html structure

---
+++ assets/js/giphyAjax.js
@@ -0,0 +1,23 @@
+/*giphy key = dc6zaTOxFJmzC*/
+
+//ajax call function
+
+
+//transform data function
+
+
+//form functionality
+	//get user input
+	//validate user input
+	//call ajax function
+
+//button functionality
+	//on click
+	//call ajax function
+
+//scroll to top button
+	//mouseOver
+
+//scroll down one page
+	//mouseOver (related to scroll to top)
+
