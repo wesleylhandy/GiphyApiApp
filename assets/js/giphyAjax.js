f405c83065476c37e64d4aa0dbad229fbda7629c
Wesley Handy <wesleylhandy@gmail.com>
2016-12-26 21:49:31 -0500

    more validation of user input and changed color theme

--- assets/js/giphyAjax.js
+++ assets/js/giphyAjax.js
@@ -43,8 +43,10 @@ function renderButtons() {
     		hue: 'orange'
     	})
 
-      $("#categories").append($("<button>").text(buttonList[i])
-      	.addClass("btn btn-default categories").data("term", buttonList[i])
+      $("#categories").append($("<button>")
+      	.text(buttonList[i])
+      	.addClass("btn btn-default categories")
+      	.data("term", buttonList[i])
       	.css("background-color", color));
     }
 
@@ -55,11 +57,12 @@ $(document).on("click", "#submit-button", function(event) {
 	event.preventDefault();
 	$(".alert").remove();
 	//get search term and save to a variable
-	let term = $("#search-text").val().trim();
+	let term = $("#search-text").val().trim().replace(" ","+").replace(/[^A-Za-z0-9_+]/g,"");
 	$("#search-text").val(''); //clear search form
 
 	//validate user input
-	if (buttonList.indexOf(term) === -1 && term !== '') {
+	if (buttonList.indexOf(term) === -1 
+		&& (term !== '' && term !== "+" && term !== "_")) {
 
 		callAPI(2, term); //make ajax call
 		
@@ -90,6 +93,7 @@ $(document).on("click", ".categories", function(){
 function callAPI(epIndex, term) {
 	queryURL = endPoints[epIndex] + term + parameters + key;
 	console.log(queryURL);
+	//ajax call
 	$.ajax({
 		url: queryURL,
 		method: "GET"
@@ -98,49 +102,55 @@ function callAPI(epIndex, term) {
 		console.log(response);
 		//empty current contents of screen to add new gifs
 		$("#results").empty();
+		// set to trending (will only function on opening screen)
 		if (term === '') {
 			term = "trending";
 		}
+		//tell user what results are currently displayed
 		$("#results").append($("<h5>").text("Now Showing: " + term.toUpperCase())
 			.css("color", "white"));
 		//add new elements to DOM
 		for (let x = 0; x < response.data.length; x++) {
 			//use randomColor.js to generate a random bright color
 			let color = randomColor({
-				luminosity: 'light',
-				hue: 'red'
+				luminosity: 'bright',
+				format: 'rgba',
+				alpha: 0.5
 			});
 			console.log(color);
 			//append this element to DOM
 			$("#results").append($("<img>")
 				.addClass("result img-rounded")
 				.attr("src", response.data[x].images.fixed_height_still.url)
-				.css("box-shadow", "0px 0px 40px 20px " + color)
+				.css("border", "4px solid " + color)
 				.data("orig", response.data[x].images.fixed_height_still.url)
 				.data("gif", response.data[x].images.fixed_height.url)
 				.data("mode", "0"));
 		}
-		
-
-
 	}).fail(function(response) {
 		//display error message
 		console.log("Giphy query error!");
 		alert("Search Error!");
-
 	});
 }
 
 //gif action button
 $(document).on("click", ".result", function() {
+	let color = randomColor({
+		luminosity: 'bright',
+		format: 'rgba',
+		alpha: 1
+	});
 	//check if still image or gif showing
 	if ($(this).data("mode") == 0) {
 		//still image showing, so change to gif
 		$(this).attr("src", $(this).data("gif"))
+		.css("box-shadow", "0px 0px 10px 10px " + color)
 		.data("mode", "1");
 	} else {
 		//gif showing, so change to still image
 		$(this).attr("src", $(this).data("orig"))
+		.css("box-shadow", "none")
 		.data("mode", "0");
 	}
 });
